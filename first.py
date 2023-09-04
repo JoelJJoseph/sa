fb47ac4377ad4362962b6fec914bf156e7364c4e
Joel John Joseph <72274851+JoelJJoseph@users.noreply.github.com>
2023-09-05 00:26:46 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,77 +1,79 @@
 import json
 import random
+import streamlit as st
+import streamlit_webrtc as webrtc
 import speech_recognition as sr
 from gtts import gTTS
 import os
-import streamlit as st
-import pygame
 from PIL import Image
 
-def app():
-        
+# Initialize speech recognition
+r = sr.Recognizer()
 
-    # Specify the absolute path to the intent.json file
-    intent_file_path = './firstaid/intent.json'  # Replace with the actual path
+# Specify the absolute path to the intent.json file
+intent_file_path = './firstaid/intent.json'  # Replace with the actual path
 
-    # Load the intent.json file
-    with open(intent_file_path, 'r') as intent_file:
-        intents = json.load(intent_file)
+# Load the intent.json file
+with open(intent_file_path, 'r') as intent_file:
+    intents = json.load(intent_file)
 
-    # Function to recognize intent
-    def recognize_intent(user_input):
-        user_input = user_input.lower()
-        for intent in intents['intents']:
-            for pattern in intent['patterns']:
-                if pattern.lower() in user_input:
-                    return intent
-        return None
+# Function to recognize intent
+def recognize_intent(user_input):
+    user_input = user_input.lower()
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            if pattern.lower() in user_input:
+                return intent
+    return None
 
-    # Function to get a random response for an intent
-    def get_response(intent):
-        responses = intent['responses']
-        return random.choice(responses)
+# Function to get a random response for an intent
+def get_response(intent):
+    responses = intent['responses']
+    return random.choice(responses)
 
-    # Function to convert text to audio and play it using pygame
-    def text_to_audio(text):
-        tts = gTTS(text=text, lang='en')
-        output_path = './firstaid/outputs'  # Replace with the actual output directory
-        os.makedirs(output_path, exist_ok=True)
-        output_file = os.path.join(output_path, 'output.mp3')
-        tts.save(output_file)
-        pygame.mixer.music.load(output_file)  # Load the correct file path
-        pygame.mixer.music.play()
+# Function to convert text to audio and play it using gTTS
+def text_to_audio(text):
+    tts = gTTS(text=text, lang='en')
+    output_path = './firstaid/outputs'  # Replace with the actual output directory
+    os.makedirs(output_path, exist_ok=True)
+    output_file = os.path.join(output_path, 'output.mp3')
+    tts.save(output_file)
+    return output_file
 
-    # Streamlit UI
-    st.title("Voice Assistant Chatbot")
-    
-    image_path = "images/first.jpg"
-    image = Image.open(image_path)
-    st.image(image)
+# Streamlit UI
+st.title("Voice Assistant Chatbot")
 
-    user_input = st.text_input("Type exit to exit with the interaction with the bot.")
+image_path = "images/first.jpg"
+image = Image.open(image_path)
+st.image(image)
 
-    if st.button("Enter Voice"):
-        if user_input.lower() == 'exit':
-            st.text("Bot: Goodbye!")
-        else:
-            try:
-                r = sr.Recognizer()
-                with sr.Microphone() as source:
-                    st.text("You (Speak):")
-                    audio = r.listen(source)
+user_input = st.text_input("Type exit to exit the interaction with the bot.")
 
-                user_input = r.recognize_google(audio)
+webrtc_ctx = webrtc.StreamlitWebRTC()
+
+if st.button("Start Voice Interaction"):
+    if user_input.lower() == 'exit':
+        st.text("Bot: Goodbye!")
+    else:
+        try:
+            st.text("You (Speak):")
+            webrtc_ctx.audio_only()
+            audio_data = webrtc_ctx.audio_receiver.get_frame()
+
+            with sr.AudioData(audio_data, webrtc_ctx.audio_receiver.sample_rate, 2):
+                user_input = r.recognize_google(audio_data)
                 st.text(f"You (Voice): {user_input}")
 
                 matched_intent = recognize_intent(user_input)
                 if matched_intent:
                     response = get_response(matched_intent)
                     st.text(f"Bot: {response}")
-                    text_to_audio(response)
+                    output_audio_file = text_to_audio(response)
+                    webrtc_ctx.audio_file(output_audio_file)
                 else:
                     st.text("Bot: I'm sorry, I didn't understand that. Please ask another question.")
 
-            except sr.UnknownValueError:
-                st.text("Bot: I couldn't understand your audio. Please try again.")
-            except sr.RequestError as e:
-                st.text(f"Bot: There was an error with the speech recognition service: {e}")
+        except sr.UnknownValueError:
+            st.text("Bot: I couldn't understand your audio. Please try again.")
+        except sr.RequestError as e:
+            st.text(f"Bot: There was an error with the speech recognition service: {e}")
